3b00b23c88322b7b3aa37501cfc07c2d555f09c9
Igor Podolskiy <igorpodolskiy82@gmail.com>
2019-11-10 16:49:29 +0200

    Updates

---
+++ precache-manifest.1763a34af8404a9c909cac09afceb723.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd08fc9a83c68d3f2bb1bdc8859811f",
+    "url": "/react-simple-game/index.html"
+  },
+  {
+    "revision": "f1e9e3ee259d7b497167",
+    "url": "/react-simple-game/static/css/main.4484d06a.chunk.css"
+  },
+  {
+    "revision": "6f03b62556d0d7e49089",
+    "url": "/react-simple-game/static/js/2.0d05ee7d.chunk.js"
+  },
+  {
+    "revision": "f1e9e3ee259d7b497167",
+    "url": "/react-simple-game/static/js/main.bd8e4b5c.chunk.js"
+  },
+  {
+    "revision": "4fd88d0dbf56560cc1cc",
+    "url": "/react-simple-game/static/js/runtime-main.cb3b3d79.js"
+  }
+]);
